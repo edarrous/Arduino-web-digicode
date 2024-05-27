4a19565c73f1e1819c00e232cba65a4c36f0a3ef
edarrous <121054272+edarrous@users.noreply.github.com>
2024-05-28 00:14:47 +0200

    change html page language

--- step-code/local_web_server_high_led.v2__prj_obj_co/local_web_server_high_led.v2__prj_obj_co.ino
+++ step-code/local_web_server_high_led.v2__prj_obj_co/local_web_server_high_led.v2__prj_obj_co.ino
@@ -86,7 +86,7 @@ void loop() {
 
             if (outputState == "on") {
               digitalWrite(outputPin, HIGH);
-              client.println("<!DOCTYPE html><html lang=\"en\"><head> <meta charset=\"UTF-8\"> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"> <title>Code correct</title></head><body> <h1>Code bon !</h1> <a href=\"/home\"><button>retour</button></a></body></html>");
+              client.println("<!DOCTYPE html><html lang=\"fr\"><head> <meta charset=\"UTF-8\"> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"> <title>Code correct</title></head><body> <h1>Code bon !</h1> <a href=\"/home\"><button>retour</button></a></body></html>");
             } 
             if (outputState == "off"){
               digitalWrite(outputPin, LOW);
